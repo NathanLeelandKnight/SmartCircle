83afd46c33bf6895cb1fa7f30def1f690219d41a
Nathan Knight <36433517+NathanLeelandKnight@users.noreply.github.com>
2019-04-12 15:56:02 -0700

    Add files via upload

---
+++ PowerShell/DHCP/EnableDHCPWin7SingleComputer.ps1
@@ -0,0 +1,15 @@
+﻿$Computers = Get-ADComputer -filter * | Select -Property Name
+Foreach($Computer in $Computers)
+{
+    $adapters = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $Computer.Name `
+    | where{$_.IPEnabled -eq 1} `
+    | where{$_.DHCPEnabled -eq 0}
+
+    if(($adapters | Measure-Object | Select -Property Count).Count -gt 0)
+    {
+        Write-Host $Computer.Name
+        $Computer.Name | Out-File -FilePath C:\Users\nknight\Desktop\HitComps.txt -append
+    }
+
+}
+
