5a80a2a35e987b4b3986f9e5318c680edf4b3531
Nathan Knight <36433517+NathanLeelandKnight@users.noreply.github.com>
2019-04-12 13:14:34 -0700

    for all computers

--- PowerShell/DHCP/EnableDHCPWin7SingleComputer.ps1
+++ PowerShell/DHCP/EnableDHCPWin7SingleComputer.ps1
@@ -1,8 +1,12 @@
-﻿$adapters = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName 10.1.3.99 `
-| where{$_.IPEnabled -eq 1} `
-| where{$_.DHCPEnabled -eq 0}
+﻿$Computers = Get-ADComputer -filter * | where{$_.Name -like "GWW-DURONSAR2"} | Select -Property Name
+Foreach($Computer in $Computers){
+    $adapters = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $Computer.Name `
+    | where{$_.IPEnabled -eq 1} `
+    | where{$_.DHCPEnabled -eq 0}
 
-Foreach($adapter in $adapters){
-    $adapter.EnableDHCP()
-    $adapter.SetDNSServerSearchOrder()
+    Foreach($adapter in $adapters){
+        $adapter.EnableDHCP()
+        $adapter.SetDNSServerSearchOrder()
+    }
 }
+
