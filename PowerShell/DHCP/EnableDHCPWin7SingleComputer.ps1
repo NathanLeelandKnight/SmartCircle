ce436399d04e7f927c693d989e25a3b9963c4bbf
Nathan Knight <36433517+NathanLeelandKnight@users.noreply.github.com>
2019-04-12 11:47:45 -0700

    DHCP

---
+++ PowerShell/DHCP/EnableDHCPWin7SingleComputer.ps1
@@ -0,0 +1,8 @@
+﻿$adapters = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName 10.1.3.99 `
+| where{$_.IPEnabled -eq 1} `
+| where{$_.DHCPEnabled -eq 0}
+
+Foreach($adapter in $adapters){
+    $adapter.EnableDHCP()
+    $adapter.SetDNSServerSearchOrder()
+}
