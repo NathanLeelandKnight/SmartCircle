c69488d3acc9b1296acf2a6239c2d01ca632a67f
Nathan Knight <nknight@thesmartcircle.com>
2019-04-15 16:00:30 -0700

    Made scripts slightly more robust and added comments for the future

--- PowerShell/DHCP/EnableDHCP.ps1
+++ PowerShell/DHCP/EnableDHCP.ps1
@@ -1,12 +1,34 @@
-﻿$Computers = Get-ADComputer -filter * | where{$_.Name -like "GWW-DURONSAR2"} | Select -Property Name
-Foreach($Computer in $Computers){
+﻿#Get the computers that you want to change
+#Example
+#$Computers = Get-Content C:\Users\nknight\Desktop\HitComps.txt
+
+#Loop through each computer and turn on DHCP
+foreach($Computer in $Computers){
+
+    #Get Network Adapters from current computer that have IP enabled and DHCP disabled
     $adapters = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $Computer.Name `
     | where{$_.IPEnabled -eq 1} `
     | where{$_.DHCPEnabled -eq 0}
 
-    Foreach($adapter in $adapters){
-        $adapter.EnableDHCP()
-        $adapter.SetDNSServerSearchOrder()
+    $RestartReq = 0
+
+    #If there are adapters present, turn on DHCP and Set DNS
+    foreach($adapter in $adapters){
+        $RetValDHCP = $adapter.EnableDHCP()
+        $RetValDNS = $adapter.SetDNSServerSearchOrder()
+
+        #Check if restart required
+        if((RetValDHCP -eq 1) -or (RetValDNS -eq 1)){
+            $RestartReq = 1
+        }
     }
+
+    #Uncomment to restart computers that require a restart
+    #Warning! This will restart regardless of who is logged on
+    <#
+    if(RestartReq -eq 1){
+        Restart-Computer -ComputerName $Computer -Force
+    }
+    #>
 }
 
--- PowerShell/DHCP/WhichCompsWillEnableDHCP.ps1
+++ PowerShell/DHCP/WhichCompsWillEnableDHCP.ps1
@@ -1,12 +1,20 @@
-﻿$Computers = Get-ADComputer -filter * | Select -Property Name
-Foreach($Computer in $Computers)
+﻿#Get every computer in AD with adjustable logon date.
+#Currently adjusted to no more than 1 years old
+#Used to not waste time trying to contact decommissioned computers.
+
+$Computers = Get-ADComputer -Filter * -Properties LastLogonDate `
+| Where {$_.LastLogonDate -gt (Get-Date).AddYears(-1)} `
+| Select -Property Name
+
+#Run script on each computer
+foreach($Computer in $Computers)
 {
-    Write-Host $Computer.Name
-    
+    #Get Network Adapters from current computer that have IP enabled and DHCP disabled   
     $adapters = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $Computer.Name `
     | where{$_.IPEnabled -eq 1} `
     | where{$_.DHCPEnabled -eq 0}
 
+    #If there are adapters present, put the name of the computer in a file "HitComps.txt"
     if(($adapters | Measure-Object | Select -Property Count).Count -gt 0)
     {
         $Computer.Name | Out-File -FilePath C:\Users\nknight\Desktop\HitComps.txt -append
