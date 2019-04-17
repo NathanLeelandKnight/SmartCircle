e804f149a401a9cdb6e1ee651acaf6969a80fc3c
Nathan Knight <nknight@thesmartcircle.com>
2019-04-17 11:15:05 -0700

    Script to find all computers and their OS

---
+++ PowerShell/GetCompsAndOS.ps1
@@ -0,0 +1,10 @@
+﻿$Computers = Get-ADComputer -Filter * -Properties LastLogonDate `
+| Where {$_.LastLogonDate -gt (Get-Date).AddMonths(-2)} `
+| Select -ExpandProperty Name
+
+foreach($Computer in $Computers){
+    $Caption = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $Computer | select -ExpandProperty Caption
+
+    New-Object PSObject -Property @{'ComputerName'="$Computer"; 'Caption'="$Caption"} `
+    | Export-Csv C:\Users\nknight\Desktop\CompsAndOS.csv -append -notypeinformation
+}
