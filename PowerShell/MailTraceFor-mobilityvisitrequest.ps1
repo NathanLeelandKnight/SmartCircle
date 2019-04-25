0cf67c1a7dbc57bcb37cdccff09ff4a390680154
Nathan Knight <nknight@thesmartcircle.com>
2019-04-25 11:32:07 -0700

    Made script more general and added comments on how to use

--- PowerShell/MailTraceFor-mobilityvisitrequest.ps1
+++ PowerShell/MailTraceFor-mobilityvisitrequest.ps1
@@ -1,4 +1,14 @@
-﻿$UserCredential = Get-Credential
+﻿<#
+This mail trace can be used for any email address.
+It can also be configured to be sent from and to specific people.
+Change $From, $To, $TraceEmail to your needs.
+#>
+
+$TraceEmail = "mobilityvisitrequest@thesmartcircle.com"
+$From       = "nknight@thesmartcircle.com"
+$To         = "tgresham@thesmartcircle.com"
+
+$UserCredential = Get-Credential
 $Session = New-PSSession    -ConfigurationName Microsoft.Exchange `
                             -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
                             -Credential $UserCredential `
@@ -7,22 +17,20 @@ $Session = New-PSSession    -ConfigurationName Microsoft.Exchange `
 
 Import-PSSession $Session -DisableNameChecking
 
-Get-Messagetrace -EndDate (Get-Date) -StartDate (Get-Date).AddDays(-9) -RecipientAddress mobilityvisitrequest@thesmartcircle.com `
+Get-Messagetrace -EndDate (Get-Date) -StartDate (Get-Date).AddDays(-9) -RecipientAddress $TraceEmail `
     | Select -property Received,SenderAddress,RecipientAddress `
     | Export-Csv "C:\Users\nknight\OneDrive - The Smart Circle\Desktop\mailTraceRec.csv"
 
-Get-Messagetrace -EndDate (Get-Date) -StartDate (Get-Date).AddDays(-9) -SenderAddress mobilityvisitrequest@thesmartcircle.com `
+Get-Messagetrace -EndDate (Get-Date) -StartDate (Get-Date).AddDays(-9) -SenderAddress $TraceEmail `
     | Select -property Received,SenderAddress,RecipientAddress `
     | Export-Csv "C:\Users\nknight\OneDrive - The Smart Circle\Desktop\mailTraceSent.csv"
 
-$From = "nknight@thesmartcircle.com"
-$To = "tgresham@thesmartcircle.com"
-$AttachmentRec = "C:\Users\nknight\OneDrive - The Smart Circle\Desktop\mailTraceRec.csv"
+$AttachmentRec  = "C:\Users\nknight\OneDrive - The Smart Circle\Desktop\mailTraceRec.csv"
 $AttachmentSent = "C:\Users\nknight\OneDrive - The Smart Circle\Desktop\mailTraceSent.csv"
-$Subject = "Mail Trace"
-$Body = "Hey Trey, here is the weekly received and sent mail trace for mobilityvisitrequest@thesmartcircle.com"
-$SMTPServer = "smtp.office365.com"
-$SMTPPort = "587"
+$Subject        = "Mail Trace"
+$Body           = "Hello, here is the weekly received and sent mail trace for mobilityvisitrequest@thesmartcircle.com"
+$SMTPServer     = "smtp.office365.com"
+$SMTPPort       = "587"
 
 Send-MailMessage -From $From -to $To -Subject $Subject -Body $Body -SmtpServer $SMTPServer -Port $SMTPPort -UseSsl `
     -Credential $UserCredential -Attachments $AttachmentRec,$AttachmentSent -DeliveryNotificationOption OnSuccess
