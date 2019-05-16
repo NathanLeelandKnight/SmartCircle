13198da6087c6d76eeb75857af1e86d8b50f4a37
Nathan Knight <nknight@thesmartcircle.com>
2019-05-16 11:13:11 -0700

    add scripts to repo

---
+++ PowerShell/MDT/Permissions/Set-MDTpermissions.ps1
@@ -0,0 +1,21 @@
+# Check for elevation
+Write-Host "Checking for elevation"
+ 
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Warning "Oupps, you need to run this script from an elevated PowerShell prompt!`nPlease start the PowerShell prompt as an Administrator and re-run the script."
+    Write-Warning "Aborting script..."
+    Break
+}
+ 
+# Configure NTFS Permissions for the MDT Build Lab deployment share
+$DeploymentShareNTFS = "C:\MDTBuildLab"
+icacls $DeploymentShareNTFS /grant '"Users":(OI)(CI)(RX)'
+icacls $DeploymentShareNTFS /grant '"Administrators":(OI)(CI)(F)'
+icacls $DeploymentShareNTFS /grant '"SYSTEM":(OI)(CI)(F)'
+ 
+# Configure Sharing Permissions for the MDT Build Lab deployment share
+$DeploymentShare = "MDTBuildLab$"
+Grant-SmbShareAccess -Name $DeploymentShare -AccountName "EVERYONE" -AccessRight Change -Force
+Revoke-SmbShareAccess -Name $DeploymentShare -AccountName "CREATOR OWNER" -Force
