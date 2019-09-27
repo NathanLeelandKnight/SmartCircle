4c33eef2d871c1e09d2939d78c18de3546619b67
Nathan Knight <nathan.leeland.knight@gmail.com>
2019-09-27 11:06:31 -0700

    adding base file

---
+++ PowerShell/KickIdleUsersSharePoint.ps1
@@ -0,0 +1,33 @@
+﻿#Connect-SPOService -Url "https://thesmartcircle-admin.sharepoint.com"
+
+#$UserCredential = Get-Credential
+
+<#$Session = New-PSSession `
+                -ConfigurationName Microsoft.Exchange `
+                -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
+                -Credential $UserCredential `
+                -Authentication Basic `
+                -AllowRedirection#>
+
+#Import-PSSession $Session -DisableNameChecking
+
+$ExternalSites = get-sposite `
+                    | Where-Object {$_.SharingCapability -eq "ExternalUserSharingOnly"} `
+                    | select -Property Url
+
+foreach($Site in $ExternalSites){
+    Write-Host "Finding activity for site: " $Site.Url
+
+    Search-UnifiedAuditLog `
+        -ObjectIds $Site.Url `
+        -StartDate 9/20/2019 `
+        -EndDate 9/29/2019 `
+        -ResultSize 5000 `
+        | select -Property UserIds `
+        | Export-Csv -Append C:\temp\sharepointtest.csv
+
+    Write-Host "Completed"
+    Write-Host
+}
+
+#Remove-PSSession $Session
