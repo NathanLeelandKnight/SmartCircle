66ec57b30e0000e05172b1abb9071904e7f986a9
Nathan Knight <nathan.leeland.knight@gmail.com>
2020-03-05 11:17:35 -0800

    Basic Get finished. Now need to reveive more instructions to continue

--- PowerShell/lastScript.ps1
+++ PowerShell/lastScript.ps1
@@ -1,1 +1,6 @@
-#Use MSoline and AzureAD Modules
+#Requires -Modules AzureAD, MSOline
+
+Connect-AzureAD
+Connect-MsolService
+
+Get-MsolUser -All | Where-Object {$_.UserType -ne "Guest" -and $_.BlockCredential -eq $False -and $_.isLicensed -eq $False} | Sort-Object -Property DisplayName
